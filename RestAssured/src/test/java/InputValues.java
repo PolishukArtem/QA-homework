f107f792bec5d29a24cdf4e404330b2927fa7e7f
PolishukArtem <CazeOne1@gmail.com>
2020-04-21 21:53:13 +0300

    Upload Rest-Assured HW

---
+++ RestAssured/src/test/java/InputValues.java
@@ -0,0 +1,33 @@
+public class InputValues {
+    public static String CreateUserList = "[\n    " +
+            "{\n        " +
+            "\"id\": 1,\n        " +
+            "\"username\": \"ahsoka\",\n        " +
+            "\"firstName\": \"Stas\",\n        " +
+            "\"lastName\": \"Syrovaten\",\n        " +
+            "\"email\": \"SS@gmailc.om\",\n        " +
+            "\"password\": \"322cashisntaproblem\",\n        " +
+            "\"phone\": \"555-222-666\",\n        " +
+            "\"userStatus\": 0\n    },\n" +
+            "{\n        " +
+            "\"id\": 2,\n        " +
+            "\"username\": \"Nawezac\",\n        " +
+            "\"firstName\": \"Artem\",\n        " +
+            "\"lastName\": \"Polishuk\",\n        " +
+            "\"email\": \"cazeone@gmail.com\",\n        " +
+            "\"password\": \"anemnogolitebenado\",\n        " +
+            "\"phone\": \"razdvatri\",\n        " +
+            "\"userStatus\": 0\n    " +
+            "}\n]";
+
+    public static String UpdateUser = "{\n        " +
+            "\"id\": 2,\n        " +
+            "\"username\": \"CazeOne\",\n        " +
+            "\"firstName\": \"Artem\",\n        " +
+            "\"lastName\": \"Polishuk\",\n        " +
+            "\"email\": \"cazeone@gmail.com\",\n        " +
+            "\"password\": \"anemnogolitebenado\",\n        " +
+            "\"phone\": \"razdvatri\",\n        " +
+            "\"userStatus\": 0\n    " +
+            "}";
+}
